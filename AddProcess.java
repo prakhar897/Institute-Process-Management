4a5c9915a12c032b8babb67520f863aafbe37362
Prakhar Gupta <prakhar897@gmail.com>
2018-11-16 00:02:18 +0530

    ff

--- AddProcess.java
+++ AddProcess.java
@@ -3,7 +3,7 @@ public class AddProcess{
 	public static void main(String[] args) {  
 		JFrame f=new JFrame();//creating instance of JFrame  
 
-		JLabel l1,l2,l3,l4;  
+		JLabel l1,l2,l3;  
 		l1=new JLabel("Add Process Page");  
     	l1.setBounds(100,0, 200,30);
     	f.add(l1);
@@ -12,25 +12,19 @@ public class AddProcess{
     	l2.setBounds(0,100,200,30);
     	f.add(l2);
 
-    	l3=new JLabel("Subject");  
+    	l3=new JLabel("Description");  
     	l3.setBounds(0,150,200,30);
     	f.add(l3);
 
-    	l4=new JLabel("Owner Email ID");  
-    	l4.setBounds(0,200,200,30);
-    	f.add(l4);
 
-		JTextField t1,t2,t3;  
+		JTextField t1,t2;  
 	    t1=new JTextField("");  
 	    t1.setBounds(180,100, 200,30);  
 	    t2=new JTextField("");  
 	    t2.setBounds(180,150, 200,30); 
-	    t3=new JTextField("");  
-	    t3.setBounds(180,200, 200,30); 
 
 	    f.add(t1);
-	    f.add(t2);
-	    f.add(t3); 
+	    f.add(t2); 
 		          
 		JButton b=new JButton("Submit");//creating instance of JButton  
 		b.setBounds(130,300,100, 40);//x axis, y axis, width, height  
