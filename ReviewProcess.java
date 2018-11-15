c42a81e4e4af303cd62ef1aff5e900911f5d85ee
Prakhar Gupta <prakhar897@gmail.com>
2018-11-15 23:45:38 +0530

    review page

--- ReviewProcess.java
+++ ReviewProcess.java
@@ -1,11 +1,11 @@
 import javax.swing.*;  
-public class Home{  
+public class ReviewProcess{  
 	public static void main(String[] args) {  
 		JFrame f=new JFrame();//creating instance of JFrame  
 
 		JLabel l1,l2,l3,l4;  
-		l1=new JLabel("Home Page");  
-    	l1.setBounds(100,0, 100,30);
+		l1=new JLabel("Review Process Page");  
+    	l1.setBounds(100,0, 200,30);
     	f.add(l1);
 
     	l2=new JLabel("Processes");  
@@ -15,25 +15,27 @@ public class Home{
     	l3=new JLabel("Dandiya Night");  
     	l3.setBounds(0,100,100,30);
     	f.add(l3);
-    	JButton b3=new JButton("Participate");  
-    	b3.setBounds(300,100,100,30);
+    	JButton b3=new JButton("Approve");  
+    	b3.setBounds(200,100,100,30);
     	f.add(b3);
+    	JButton b5=new JButton("Reject");  
+    	b5.setBounds(300,100,100,30);
+    	f.add(b5);
 
     	l4=new JLabel("Conference");  
     	l4.setBounds(0,150,100,30);
     	f.add(l4);
-    	JButton b4=new JButton("Participate");  
-    	b4.setBounds(300,150,100,30);
+    	JButton b4=new JButton("Approve");  
+    	b4.setBounds(200,150,100,30);
     	f.add(b4); 
+    	JButton b6=new JButton("Reject");  
+    	b6.setBounds(300,150,100,30);
+    	f.add(b6);
 		          
-		JButton b1=new JButton("Review Process"); 
+		JButton b1=new JButton("Back To Home Page"); 
 		b1.setBounds(50,300,200, 40);  
-
-		JButton b2=new JButton("Add Process"); 
-		b2.setBounds(50,350,200, 40); 
 		          
 		f.add(b1);
-		f.add(b2);
 		          
 		f.setSize(400,500); 
 		f.setLayout(null); 
